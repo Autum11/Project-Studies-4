ee66f8429f344d089aa0fb53af27abd849d242d4
LAPTOP-4QV3I7GN\sst_h <t4tish00@students.oamk.fi>
2016-12-09 08:54:03 +0200

    _theme file structure

---
+++ app/code/Dhimal/Theme/Controller/Index/index.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Dhimal\Theme\Controller\Index;
+
+class Index extends \Magento\Framework\app\Action\Action
+{
+    public function __construct(\Magento\Framework\App\Action\Context)
+    public function execute()
+    {
+        echo 'Wroking';
+    }
+}
